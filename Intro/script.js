e499b7847794ba52be2055a57ead3ca384b1968c
Mars Alexis Regidor <pythonista.mars@gmail.com>
2026-05-22 15:46:16 +0800

    added apikey

--- Intro/script.js
+++ Intro/script.js
@@ -4,11 +4,12 @@ require(["esri/config", "esri/Map", "esri/views/MapView"], function (
   MapView,
 ) {
   // Set your API key
-  esriConfig.apiKey = "YOUR_API_KEY";
+  esriConfig.apiKey =
+    "AAPTaJYkH5ASSLWvdKQWeQ8cs5Q..JmR5E1jJCVswiUjDOD07Wfgyesr87fOEEparDrWmYzJRv8_mIZXixUdW6w1OxvvckCGEgkPO-zZvE8B1QkYxJiSujtj3D0k2rzxDdqW1XZyV1W_cjxWl3UKfyxiNbi13vIjFxMZxZIs9AeCRiRXtxxG-yMJa6kKzD9NkU6qh9bsoyG8UwbEKJfL59DitldgFK8GT9idjo5Hj2OBB-jrWDdIq8NDQIMJCmPCy4nxlgwr8UABdhQKrPrebGBU.AT1_U02ceave";
 
   // Create a map with a topographic basemap
   const map = new Map({
-    basemap: "hybrid",
+    basemap: "arcgis-streets",
   });
 
   // Create a view and link it to the map
